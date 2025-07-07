bb4c4c0b24d7943f82f81a9616ba4d50b92f647a
Tatsuya Takigawa <taki0701tty@gmail.com>
2025-07-07 11:34:09 +0900

    AWS EC2サーバーで動作確認

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://ec2-3-236-219-100.compute-1.amazonaws.com:8000/predict"
+url = "http://44.222.195.104:8000//predict"
 payload = { "user_id" : "3" }
 
 response = requests.post(url, json=payload)
